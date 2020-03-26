1ebe6fe81231966b2afad4130c3b22e6d8efe166
Daniel <addodaniellarbi@gmail.com>
2020-03-26 11:27:51 +0000

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            // The `injectType`  option can be avoided because it is default behaviour
+            { loader: 'style-loader', options: { injectType: 'styleTag' } },
+            'css-loader',
+          ],
+        },
+      ],
+    },
+  };
