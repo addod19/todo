471b38c41a598b7cb2ab77b150f38f95df3076de
Daniel <addodaniellarbi@gmail.com>
2020-03-30 15:36:19 +0000

    uikit setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
