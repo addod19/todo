9680603a930b3c0b713fdf1e887a3c47b99637bc
macnick <nick@macexperts.gr>
2020-03-30 20:27:03 +0300

    Add uikit and default todo description

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [
-            // The `injectType`  option can be avoided because it is default behaviour
-            { loader: 'style-loader', options: { injectType: 'styleTag' } },
-            'css-loader',
-          ],
-        },
-      ],
-    },
-  };
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
